7ef03aa2bcc7a5f26f30e43f473889afdda56dc5
Brian <brian.w.tjahjadi@gmail.com>
2024-07-13 00:40:23 +1000

    about me scaffold

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -3,7 +3,15 @@ import styles from './AboutMe.module.css'
 export default function AboutMe() {
     return (
         <div className={styles.container}>
-            About Me
+            <section className={styles.text}>
+                <h1>About Me</h1>
+                <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, sed at cumque veniam eius alias incidunt cupiditate obcaecati maxime, facere odit quo nihil rem dolore nostrum saepe, impedit illo nesciunt.</h2>
+            </section>
+            <section className={styles.display}>
+                <div>
+                    Weather API call here...
+                </div>
+            </section>
         </div>
     );
 }
