8e5ccdd350367959b338123e98e2695ff62ff8be
Brian <brian.w.tjahjadi@gmail.com>
2024-07-12 23:21:35 +1000

    scaffold v3

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -3,7 +3,7 @@ import styles from './AboutMe.module.css'
 export default function AboutMe() {
     return (
         <div className={styles.container}>
-            <p>About Me</p>
+            About Me
         </div>
     );
 }
