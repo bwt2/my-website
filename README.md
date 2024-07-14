2abac95fc33649362224d0ff3e5a9a5cf9412e7e
Brian <brian.w.tjahjadi@gmail.com>
2024-07-14 13:28:52 +1000

    navbar edits

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # React + Vite
 
-Note: It is required to run this in the linux filesystem and not C:// if using WSL2 
+For deployment, run 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-For deployment:
+```
+npm run build
+git add -f dist
+git commit -m "a commit message here"
+git subtree push --prefix dist origin gh-pages
+```
 https://www.youtube.com/watch?v=yo2bMGnIKE8
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,19 +6,19 @@ export default function Navbar({ scrollToSection, refs }){
       <h1 onClick={() => scrollToSection(refs.aboutMeRef)}>
         {"<"} About Me {">"}
       </h1>
-      <NavbarConnector repeat={4}/>
+      <NavbarConnector repeat={2}/>
       <h1 onClick={() => scrollToSection(refs.experienceRef)}>
         {"<"} Experience {">"}
       </h1>
-      <NavbarConnector repeat={4}/>
+      <NavbarConnector repeat={2}/>
       <h1 onClick={() => scrollToSection(refs.skillsRef)}>
         {"<"} Skills {">"}
       </h1>
-      <NavbarConnector repeat={4}/>
+      <NavbarConnector repeat={2}/>
       <h1 onClick={() => scrollToSection(refs.projectsRef)}>
         {"<"} Projects {">"}
       </h1>
-      <NavbarConnector repeat={4}/>
+      <NavbarConnector repeat={2}/>
       <h1 onClick={() => scrollToSection(refs.contactRef)}>
         {"<"} Contact {">"}
       </h1>
